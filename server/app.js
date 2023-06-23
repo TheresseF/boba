6bb31afc59740b1ec3b8e74d686e832b69df6bef
Theresse Faith Amamangpang <tcamamangpang@up.edu.ph>
2023-06-23 16:50:16 +0800

    idk what to do with my life

--- server/app.js
+++ server/app.js
@@ -1,25 +1,37 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function(req, res){
-    console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var myReadStream = fs.createReadStream(__dirname + '../../Customer/HOMEPAGE.html', 'utf8');
-    myReadStream.pipe(res);
+//app.set('view engine', 'ejs');
 
-
-//     if(req.url === '/home' || req.url === '/'){
-//         res.writeHead(200, {'Content-Type': 'text/html'});
-//         fs.createReadStream(__dirname + '../../Customer/HOMEPAGE.html').pipe(res);
-//         // myReadStream.pipe(res);
-//     } 
-//     else if(req.url === '/order'){
-//         res.writeHead(200, {'Content-Type': 'text/html'});
-//         fs.createReadStream(__dirname + '../../Customer/Order/FINALORDER1.html').pipe(res);
-//     }
-    
+app.get('/home', function(req,res){
+    res.sendFile(__dirname + '../../Customer/HOMEPAGE.html');
 });
 
+app.listen(3000);
 
-server.listen(3000, '127.0.0.1');
-console.log('now listening to port 3000');
+
+// var http = require('http');
+// var fs = require('fs');
+
+// var server = http.createServer(function(req, res){
+//     console.log('request was made: ' + req.url);
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     var myReadStream = fs.createReadStream(__dirname + '../../Customer/HOMEPAGE.html', 'utf8');
+//     myReadStream.pipe(res);
+
+
+// //     if(req.url === '/home' || req.url === '/'){
+// //         res.writeHead(200, {'Content-Type': 'text/html'});
+// //         fs.createReadStream(__dirname + '../../Customer/HOMEPAGE.html').pipe(res);
+// //         // myReadStream.pipe(res);
+// //     } 
+// //     else if(req.url === '/order'){
+// //         res.writeHead(200, {'Content-Type': 'text/html'});
+// //         fs.createReadStream(__dirname + '../../Customer/Order/FINALORDER1.html').pipe(res);
+// //     }
+    
+// });
+
+
+// server.listen(3000, '127.0.0.1');
+// console.log('now listening to port 3000');
