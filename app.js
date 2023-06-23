0151ab9d7f433b9daec9d77328ced06e6abbab3a
Theresse Faith Amamangpang <tcamamangpang@up.edu.ph>
2023-06-23 17:50:49 +0800

    controller part 2

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.set('view engine', 'ejs');
 //static files
 app.use(express.static('./public'));
 
+//fire controllers
+homeController(app);
+
 //listen to port
 app.listen(3000);
 console.log('app is now listening to port 3000')
