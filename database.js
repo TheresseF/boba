bf39e33353447e6a6a9601c9dbdfa66b93930d73
Theresse Faith Amamangpang <tcamamangpang@up.edu.ph>
2023-06-26 17:49:01 +0800

    comment

--- database.js
+++ database.js
@@ -1,26 +1,26 @@
-import mysql from 'mysql2';
+// import mysql from 'mysql2';
 
-const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'bobastic',
-}).promise()
+// const pool = mysql.createPool({
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: '',
+//     database: 'bobastic',
+// }).promise()
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL database:', err);
-    return;
-  }
-  console.log('Connected to MySQL database');
-});
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to MySQL database:', err);
+//     return;
+//   }
+//   console.log('Connected to MySQL database');
+// });
 
-pool.query('SELECT * FROM bobastic.customer_list', (error, results, fields) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
-    console.log(results);
-});
+// pool.query('SELECT * FROM bobastic.customer_list', (error, results, fields) => {
+//     if (error) {
+//       console.error(error);
+//       return;
+//     }
+//     console.log(results);
+// });
   
-module.exports = connection;
+// module.exports = connection;
